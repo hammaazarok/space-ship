8392a82d99eb6683e73d4919d61397685652231a
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-08-22 11:00:29 +0100

    add pages and its routings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
 import Nav from './components/Nav';
+import Rockect from './components/pages/Rockect';
+import Missions from './components/pages/Missions';
+import Profile from './components/pages/Profile';
 
 function App() {
   return (
     <div className="App">
       <Nav />
-     <h1>Our Space ship</h1>
+      <Routes>
+        <Route path='/' element={<Rockect />} />
+        <Route path='/missions' element={<Missions />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/pages/Rockect.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Rockect = () => {
+  return (
+    <div>Rockect</div>
+  )
+}
+
+export default Rockect
