b05e3545b8a4303175ecca4320a104b91d1d6a62
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-08-23 14:28:15 +0100

    rendered data

--- src/components/pages/RockectDisplay.js
+++ src/components/pages/RockectDisplay.js
@@ -1,27 +1,27 @@
 /* eslint-disable camelcase */
-// import { useSelector } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 const RockectDisplay = () => {
-  // const rockects = useSelector((state) => state.rockects);
+  const rockects = useSelector((state) => state.rockects);
 
-  // const renderRockect = rockects.map((rockect) => (
-  //   <div key={rockect.id}>
-  //     <div>
-  //       <img src={rockect.flickr_images} alt={rockect.rocket_name} />
-  //     </div>
-  //     <div>
-  //       <h1>{rockect.rocket_name}</h1>
-  //       <h2>{rockect.rocket_type}</h2>
-  //       <p>
-  //         {rockect.description}
-  //       </p>
-  //     </div>
-  //   </div>
-  // ));
+  const renderRockect = rockects.map((rockect) => (
+    <div key={rockect.id}>
+      <div>
+        <img src={rockect.flickr_images} alt={rockect.rocket_name} />
+      </div>
+      <div>
+        <h1>{rockect.rocket_name}</h1>
+        <p>
+          {rockect.description}
+        </p>
+      </div>
+    </div>
+  ));
 
-  // return (
-  //   <>{ renderRockect }</>
-  // );
+  return (
+    <>{ renderRockect }</>
+  );
 };
 
 export default RockectDisplay;
