edd858a2e7df24f1b51e9c70376b09e0613b6425
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-08-23 22:34:18 +0100

    add rocket responsiveness

--- src/components/pages/RockectDisplay.js
+++ src/components/pages/RockectDisplay.js
@@ -8,42 +8,44 @@ import Button from 'react-bootstrap/Button';
 const RockectDisplay = ({
   name, description, img, id, reserved, bookingHandler,
 }) => (
-  <div className="main">
-    <div className="rocketImage">
-      <img src={img} alt="rockets" />
-    </div>
-    <div className="rocketDescription">
-      <h1>{name}</h1>
-      <p>
+  <div className="container">
+    <div className="main">
+      <div className="rocketImage">
+        <img src={img} alt="" />
+      </div>
+      <div className="rocketDescription">
+        <h1>{name}</h1>
+        <p>
+          {reserved ? (
+            <>
+              <Badge bg="primary">Reserved</Badge>
+              <span> </span>
+            </>
+          ) : (
+            <></>
+          )}
+          {description}
+        </p>
         {reserved ? (
-          <>
-            <Badge bg="primary">Reserved</Badge>
-            <span> </span>
-          </>
+          <Button
+            variant="primary"
+            className="btn btn-cancel-reserve width-20vw back-color-gray"
+            onClick={() => bookingHandler(id)}
+            id={id}
+          >
+            Cancel Reservation
+          </Button>
         ) : (
-          <></>
+          <Button
+            variant="primary"
+            className="btn btn-reserve width-20vw"
+            onClick={() => bookingHandler(id)}
+            id={id}
+          >
+            Reserve Rocket
+          </Button>
         )}
-        {description}
-      </p>
-      {reserved ? (
-        <Button
-          variant="primary"
-          className="btn btn-cancel-reserve width-20vw back-color-gray"
-          onClick={() => bookingHandler(id)}
-          id={id}
-        >
-          Cancel Reservation
-        </Button>
-      ) : (
-        <Button
-          variant="primary"
-          className="btn btn-reserve width-20vw"
-          onClick={() => bookingHandler(id)}
-          id={id}
-        >
-          Reserve Rocket
-        </Button>
-      )}
+      </div>
     </div>
   </div>
 );
