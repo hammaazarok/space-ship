d1e69e6bc76e19a2502c34b14ffd63028987b33e
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-08-23 14:57:48 +0100

    render and style the list

--- src/components/pages/RockectDisplay.js
+++ src/components/pages/RockectDisplay.js
@@ -1,16 +1,17 @@
 /* eslint-disable camelcase */
 import React from 'react';
 import { useSelector } from 'react-redux';
+import './RockectDisplay.css';
 
 const RockectDisplay = () => {
   const rockects = useSelector((state) => state.rockects);
 
   const renderRockect = rockects.map((rockect) => (
-    <div key={rockect.id}>
-      <div>
+    <div key={rockect.id} className="main">
+      <div className="rocketImage">
         <img src={rockect.flickr_images} alt={rockect.rocket_name} />
       </div>
-      <div>
+      <div className="rocketDescription">
         <h1>{rockect.rocket_name}</h1>
         <p>
           {rockect.description}
