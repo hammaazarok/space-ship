f909f59791f3309b3871130932bd6055b78441d3
Hamma Azarok <hammaagazarok@gmail.com>
2022-08-23 12:19:59 +0200

    merge dev

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import missionsReducer from './missions/missions';
+import rocketReducer from './reducers/RockectReducer';
 
 const store = configureStore({
   reducer: {
     missions: missionsReducer,
+    rockets: rocketReducer,
   },
 });
 
