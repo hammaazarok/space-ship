baa7c0437e0b7367981886be2c9e41c6b9c219b1
Oyinlade Ojesanmi <Ojesanmioyinlade@gmail.com>
2022-08-24 12:15:08 +0100

    create rockect profile

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -5,6 +5,8 @@ import { useSelector } from 'react-redux';
 const Profile = () => {
   const { missions } = useSelector((state) => state.missions);
   const Missions = missions.filter((m) => m.isMember);
+  const rockects = useSelector((state) => state.rockects);
+  const Rockects = rockects.filter((rock) => rock.reserved);
   return (
     <>
       <div className="profile">
@@ -18,6 +20,11 @@ const Profile = () => {
         </div>
         <div className="profile-rockets">
           <h2>My Rockets</h2>
+          <ListGroup>
+            {Rockects.map(((rock) => (
+              <ListGroup.Item key={rock.rockects}>{rock.rocket_name}</ListGroup.Item>
+            )))}
+          </ListGroup>
         </div>
       </div>
     </>
